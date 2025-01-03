bbc94015c0f2b6f03d635d6852be667b8f882212
Tamoghna35 <60691052+Tamoghna35@users.noreply.github.com>
2025-01-03 09:34:39 +0530

    add: the relationship between task and attatchment

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,7 @@
 
 import { sequelize } from "../db.config.js";
 import { Task } from "./task.model.js";
+import Attatchment from "./attatchment.model.js";
 // export const CONNECT_DB = async () => {
 //     try {
 //         await sequelize.authenticate()
@@ -14,10 +15,18 @@ import { Task } from "./task.model.js";
 //     }
 // }
 
+// one-to-many relationship
+Task.hasMany(Attatchment, { foreignKey: "task_id" })
+Attatchment.belongsTo(Task, { foreignKey: "task_id" })
+
+
 export const CONNECT_DB = () => {
     return sequelize
         .authenticate()
         .then(() => {
+            const t = sequelize.sync({ alter: true });
+            console.log(typeof (t));
+
             return sequelize.sync({ alter: true });
         })
         .then(() => {
@@ -29,5 +38,6 @@ export const CONNECT_DB = () => {
 };
 
 export {
-    Task
+    Task,
+    Attatchment
 }
